9be07390dd4a16abcc50e5cb3b4f117fd205c00c
Vad <vad.adler@gmail.com>
2021-10-12 09:06:54 -0700

    Added string reverse challenge and updated longest prefix challenge.

--- iq/src/arrays.kt
+++ iq/src/arrays.kt
@@ -11,6 +11,14 @@ fun longestPrefix(arr: Array<String>): String {
     arr.sort()
     println("Sorted: " + Arrays.toString(arr))
 
-    
-    return "Longest prefix: "
+    // Starting position is the length of the shortest string.
+    var pos = arr[0].length
+
+    for (i in arr.indices) {
+        if (arr[i].elementAt(pos) != arr[i + 1].elementAt(pos))
+            break
+        pos--
+    }
+
+    return "Longest prefix: " + arr[0].substring(0, pos)
 }
