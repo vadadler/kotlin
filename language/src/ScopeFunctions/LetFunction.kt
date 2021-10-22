70651a9244dc06b8c81a3e63e8fe09aba63bca0b
Vad <vad.adler@gmail.com>
2021-10-21 18:18:52 -0700

    Scope funcitons samples.

---
+++ language/src/ScopeFunctions/LetFunction.kt
@@ -0,0 +1,18 @@
+package ScopeFunctions
+
+/*
+Context object : it
+Return value   : lambda result
+Use case       : let function is often used to provide null safety calls. Use safe call operator(?.) with ‘let’ for
+                 null safety. It executes the block only with the non-null value.
+ */
+class LetFunction {
+}
+
+fun main() {
+    var v1: Int? = null
+    v1?.let { println("v1 is null and $v1 will not be printed. NPE is avoided.") }
+
+    v1 = 22
+    v1?.let { println("v1 isn not null: v1=$v1") }
+}
