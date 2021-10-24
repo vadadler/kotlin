f21e1c15da9a5c40382a1426cb532bba9b2d78f5
Vad <vad.adler@gmail.com>
2021-10-24 11:59:13 -0700

    Update SubArraySum0.kt

--- iq/src/arrays/SubArraySum0.kt
+++ iq/src/arrays/SubArraySum0.kt
@@ -10,7 +10,7 @@ class SubArraySum0 {
      * Brute force O(n^2) time complexity walk. Two loops: outer running from i and inner running from i+1 searching
      * for subarray with sum of 0.
      */
-    fun isSubarrayBrute(list: List<Int>): Boolean {
+    fun isSubarrayLoops(list: List<Int>): Boolean {
         var sum = 0
         for (i in list.indices) {
             sum = list[i]
@@ -28,10 +28,30 @@ class SubArraySum0 {
         }
         return false
     }
+
+    /*
+     * Use a MutableSet<Int> to store sums for each element list[i] calculated as sum of list elements from 0 to i.
+     * If a sum already exists in the set then there is a subarray with sum of 0. Complexity is O(n).
+     */
+    fun isSubarraySet(list: List<Int>): Boolean {
+        var sumSet = mutableSetOf<Int>()
+
+        var sum = 0
+        for (i in list.indices) {
+            sum += list[i]
+            if (sumSet.contains(sum)) {
+                return true
+            }
+            sumSet.add(sum)
+        }
+
+        return false
+    }
 }
 
 fun main() {
     val arr = listOf(2, 5,-3, -3, 6, -5, 4)
 
-    println("Is there a subarray for $arr ${SubArraySum0().isSubarrayBrute(arr)}" )
+    println("Brute force: Is there a subarray for $arr ${SubArraySum0().isSubarrayLoops(arr)}" )
+    println("Using set: Is there a subarray for $arr ${SubArraySum0().isSubarraySet(arr)}" )
 }
