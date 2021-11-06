b2500ee892e1ccdc652c9bc37c4e1851a3c120f0
Vad <vad.adler@gmail.com>
2021-11-05 18:17:15 -0700

    Update Meta.kt

--- iq/src/co/Meta.kt
+++ iq/src/co/Meta.kt
@@ -36,3 +36,12 @@ fun twoNumSum(list: List<Int>, sum: Int): List<Int> {
     }
     return ret
 }
+
+// Given an unsorted array, find 2 numbers that sum up to S.
+// The strategy here is to walk input list, for each pair of numbers sum them up and subtruct desired sum and store
+// result in a set. Walk that set and see if value of 0 is there or two values add to 0. If that's the case, then
+// there are two numbers which sum is the desired one. 
+fun twoNumSumUnsorted(list: List<Int>, sum: Int): List<Int> {
+    var ret = mutableListOf<Int>()
+    return ret
+}
