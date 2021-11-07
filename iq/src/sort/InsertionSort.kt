75a1db6f90fc685a0bf04a96b1d938d8d2a603f6
Vad <vad.adler@gmail.com>
2021-11-07 09:41:23 -0800

    Update InsertionSort.kt

--- iq/src/sort/InsertionSort.kt
+++ iq/src/sort/InsertionSort.kt
@@ -7,7 +7,7 @@ fun main() {
     var list = mutableListOf<Int>(5,2,4,6,1,3)
 
     println("Unsorted list $list")
-    insertionSort(list)
+    insertionSortReverse(list)
     println("Sorted list $list")
 }
 
@@ -20,6 +20,20 @@ fun insertionSort(list: MutableList<Int>) {
             val v2 = list[j + 1]
             list[j+1] = list[j]
             list[j] = v2
+            j -= 1
+        }
+    }
+}
+
+// Sort from largest to smallest.
+fun insertionSortReverse(list: MutableList<Int>) {
+    for(i in 1..list.size - 1) {
+        var v = list[i]
+        var j = i - 1
+        while (j >= 0 && list[j] < list[i]) {
+            var v2 = list[j]
+            list[j] = list[j + 1]
+            list[j + 1] = v2
             j -= 1
         }
     }
