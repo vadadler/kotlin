5fff4e29f84d60df2abbce606d7b3ffc5788898c
Vad <vad.adler@gmail.com>
2021-11-12 18:15:46 -0800

    Update MergeSort.kt

--- iq/src/sort/MergeSort.kt
+++ iq/src/sort/MergeSort.kt
@@ -9,8 +9,8 @@ class MergeSort {
 fun main() {
     var list = listOf<Int>(5,2,4,7,1,3,2,6)
     println("Unsorted list $list")
-    mergeSort(list)
-    println("Sorted list $list")
+    var sortedList = mergeSort(list)
+    println("Sorted list $sortedList")
 }
 
 fun mergeSort(list: List<Int>): List<Int> {
@@ -18,7 +18,7 @@ fun mergeSort(list: List<Int>): List<Int> {
 
     val middle = list.size/2
     val left = list.subList(0, middle)
-    val right = list.subList(middle + 1, list.size)
+    val right = list.subList(middle, list.size)
 
     return merge(mergeSort(left), mergeSort(right))
 }
@@ -26,7 +26,24 @@ fun mergeSort(list: List<Int>): List<Int> {
 fun merge(left: List<Int>, right: List<Int>): List<Int> {
     var indexLeft = 0
     var indexRight = 0
-    var sublist = mutableListOf<Int>()
+    var tempList = mutableListOf<Int>()
 
-    
+    while (indexLeft < left.size && indexRight < right.size) {
+        if (left[indexLeft] > right[indexRight]) {
+            tempList.add(right[indexRight++])
+        }
+        else {
+            tempList.add(left[indexLeft++])
+        }
+    }
+
+    while (indexLeft < left.size) {
+        tempList.add(left[indexLeft++])
+    }
+
+    while (indexRight < right.size) {
+        tempList.add(right[indexRight++])
+    }
+
+    return tempList
 }
