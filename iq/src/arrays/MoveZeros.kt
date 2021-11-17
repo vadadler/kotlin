ce6e3df80e7fbdfac80501ed90e5649760379d5f
Vad <vad.adler@gmail.com>
2021-11-16 18:37:19 -0800

    Update MoveZeros.kt

--- iq/src/arrays/MoveZeros.kt
+++ iq/src/arrays/MoveZeros.kt
@@ -7,16 +7,20 @@ class MoveZeros {
 fun moveZerosToBack(arr: MutableList<Int>) {
     // Keep two indexes. One pointing to element with 0 and the other !0 element.
     var i = 0 // 0 element.
-    var j = 0 // !0 element.
+    var j = 0 // where to place !0 element.
 
-    for (element in arr.indices ) {
-        if (arr[element] != 0) {
-
+    while (i < arr.size - 1) {
+        if (arr[i] != 0) {
+            arr[j] = arr[i]
+            i++
+            j++
         }
         else {
-
+            i++
         }
     }
+
+    while (j < arr.size - 1) arr[j++] = 0
 }
 
 fun moveZerosToFront(arr: MutableList<Int>) {
@@ -25,8 +29,9 @@ fun moveZerosToFront(arr: MutableList<Int>) {
 
 fun main() {
     var arr = mutableListOf<Int>(0,2,1,0,6,0)
-    print("Array before: $arr")
-    print("Array after: ${moveZerosToBack(arr)}")
-    print("Array after 2: ${moveZerosToFront(arr)}")
+    println("Array before: $arr")
+    moveZerosToBack(arr)
+    println("Array after: $arr")
+//    print("Array after 2: ${moveZerosToFront(arr)}")
 }
 
