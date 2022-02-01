14165f97715f0838c358bca01a5e389444846603
Vad <vad.adler@gmail.com>
2022-01-31 18:31:19 -0800

    Work on LinkedList

--- iq/src/datastructures/LinkedList.kt
+++ iq/src/datastructures/LinkedList.kt
@@ -5,28 +5,56 @@ class LinkedList<T: Any> {
     var tail: Node<T>? = null
     var size = 0
 
-    // Add node in the front.
+    // Add node at the front/head.
     fun push(value: T) {
-        var n = Node(value, head)
+        val n = Node(value, head)
 
-        if (head != null) {
-            tail = head
+        if(head == null) {
+            head = n
+        } else {
+            n.next = head
+            head = n
         }
-
-        head = n
 
         size++
     }
 
-    // Add node at the end.
+    // Append value at the tail.
     fun append(value: T) {
-        var n = Node(value, null)
+        val n = Node(value, null)
 
-        if (head != null) {
+        if (head == null) {
+            push(value)
+        } else if (tail == null) {
+            head?.next = n
+            tail = n
+        } else {
             tail?.next = n
             tail = n
         }
+
         size++
+    }
+
+    // Insert node at a position in the list.
+    fun insertAt(pos: Int, n: Node<T>) {
+        var nodeToInsertAt = head
+
+        // A little optimization.
+        when (pos) {
+            0 -> { push(n.value) }
+            (size - 1) -> { append(n.value) }
+            else -> {
+                for (i in 0..pos) {
+                    nodeToInsertAt = nodeToInsertAt?.next
+                }
+
+                val nodeTemp = nodeToInsertAt?.next
+                nodeToInsertAt?.next = n
+                n.next = nodeTemp
+
+            }
+        }
 
     }
 
@@ -54,19 +82,16 @@ data class Node<T: Any>(var value: T, var next: Node<T>?) {
 }
 
 fun main() {
-//    val n4 = Node(4, null)
-//    val n3 = Node(3, n4)
-//    val n2 = Node(2, n3)
-//    val n1 = Node(1, n2)
-
-//    print(n1)
-
     var ll = LinkedList<Int>()
-    ll.append(1)
-    ll.push(2)
-    ll.push(3)
-    ll.append(0)
-    ll.push(4)
+    with(ll) {
+        push(2)
+        append(1)
+        push(3)
+        append(6)
+        push(4)
 
-    print(ll)
+        println("Before insert $this")
+        insertAt(2, Node(7, null))
+        println("After insert $this")
+    }
 }
---
+++ language/src/collections/Set.kt
@@ -0,0 +1,10 @@
+package collections
+
+class Set {
+}
+
+fun main() {
+    val l = listOf(120, 131, 120, 175, 190)
+    val m = l.groupBy{it}.filter { e -> e.value > 1 }
+//    val set = listOf(120, 131, 120, 175, 190).toSet().filter { v -> v. }
+}
