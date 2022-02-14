9d7e3eff4e780c82e198e72f5cdd556badd521e3
Vad <vad.adler@gmail.com>
2022-02-14 12:53:46 -0800

    Update Set.kt

--- language/src/collections/Set.kt
+++ language/src/collections/Set.kt
@@ -5,6 +5,6 @@ class Set {
 
 fun main() {
     val l = listOf(120, 131, 120, 175, 190)
-    val m = l.groupBy{it}.filter { e -> e.value > 1 }
+//    val m = l.groupBy{it}.filter { e -> e.value > 1 }
 //    val set = listOf(120, 131, 120, 175, 190).toSet().filter { v -> v. }
 }
