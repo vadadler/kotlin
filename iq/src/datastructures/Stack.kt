2288241e53ad6eb5fdded8d039da5cd079696beb
Vad <vad.adler@gmail.com>
2022-02-18 18:29:51 -0800

    Stack changes.

--- iq/src/datastructures/Stack.kt
+++ iq/src/datastructures/Stack.kt
@@ -1,28 +1,51 @@
 package datastructures
 
+import kotlin.jvm.internal.Intrinsics
+import kotlin.Int
+
 interface IStack<Int> {
     fun push(element: Int)
     fun pop(): Int?
     fun isEmpty(): Boolean
-    fun size(): Int
+    fun size(): kotlin.Int
 }
 
 class Stack<Int>:IStack<Int> {
-
+    var data = arrayListOf<Int>()
     var head: Int? = null
-    var size: Int = 0
+    var size = 0
 
     public override fun push(value: Int) {
-
+        if (size == 0) {
+            data.add(value)
+            head = value
+        }
+        size++
     }
 
     public override fun pop(): Int? {
-        return head ?: null
+        var retVal: Int? = null
+        if (head != null) {
+            retVal = head
+            data.removeAt(size)
+            size--
+        }
+        return retVal
     }
 
     public override fun isEmpty(): Boolean {
         return size == 0
     }
 
-    public override fun size(): Int = size
+    public override fun size(): kotlin.Int = size
+
+    public override fun toString(): String {
+
+    }
+}
+
+fun main() {
+    var st = Stack<Int>()
+    st.push(1)
+
 }
